8865215518619bd10324be8f5c53b09b6ad83746
Gabriel Surculento <gespejo121@gmail.com>
2021-12-10 20:00:55 -0300

    Usando Express 2

--- index.js
+++ index.js
@@ -2,16 +2,94 @@ const express = require('express')
 
 const app = express();
 
-const PORT = process.env.PORT|| 8050;
+const PORT = process.env.PORT|| 8080;
+const fs = require('fs');
+
+class Contenedor {
+  constructor(nombre){
+    this.nombre = `./${nombre}.txt`;
+    this.items = [];
+  }
+  async open(){
+    try {
+      if (fs.existsSync(this.nombre)) {
+        this.items = JSON.parse(fs.readFileSync(this.nombre,'utf-8'));
+      } else {
+        fs.writeFileSync(this.nombre,JSON.stringify([]),'utf-8');
+      }
+    } catch (error) {}
+  }
+  async save(producto){
+    try {
+      this.items.push({id:this.length, title:producto.title, price:producto.price, id: (this.items.length + 1),thumbnail: producto.img});
+      await fs.promises.writeFile(this.nombre,JSON.stringify(this.items,null, 2));
+      return this.items.length;
+      }
+      catch(error) {
+        console.log(error.message);
+      }
+  }
+  async getById(id){
+    try {
+      const info = await this.getAll()
+      const obj = (info != undefined) ? info.find(o => o.id == id) : [];
+      console.log(obj);
+      return obj;
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async getAll(){
+    try {
+      if (fs.existsSync(this.nombre)) {
+        const datos = await fs.promises.readFile(this.nombre, 'utf-8')
+        return JSON.parse(datos);
+      }
+      
+      return undefined
+    } catch (error) {
+      console.log('error');
+    }
+  }
+
+  async deleteById(id){
+    try {
+      const info = await this.getAll()
+      const obj = (info != undefined) ? info.filter(o => o.id !== id) : info;
+      console.log(obj);
+      return await fs.promises.writeFile(this.nombre,JSON.stringify(obj,null, 2));
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async deleteAll(){
+    await fs.promises.unlink(this.nombre)
+  }
+}
+const load = async () => {
+  const id = await productos.save({title:'regla',price: 7, img: 'https://upload.wikimedia.org/wikipedia/commons/3/3f/Regla_H.svg'});
+  console.log(id);
+}
+
+const productos = new Contenedor("productos");
+productos.open()
+// load();
+// productos.getById(3);
+// productos.getAll();
+// productos.deleteById(2);
+// productos.deleteAll();
 
 app.get('/', (req, res) => {
   res.send('<h1 style="color:blue">Bienvenidos al servidor express</h1>')
 })
-app.get('/visitas', (req, res) => {
-  res.send({mensaje: 'Hola desde express'})
+app.get('/productos', (req, res) => {
+  res.send(productos.items)
 })
-app.get('/fyh', (req, res) => {
-  res.send({mensaje: 'Hola desde express'})
+app.get('/productoRandom', (req, res) => {
+  const idRandom = Math.floor((productos.items.length + 1)*(Math.random()))
+  res.send(productos.items[idRandom])
 })
 
 const connectedServer = app.listen(PORT, () => {
